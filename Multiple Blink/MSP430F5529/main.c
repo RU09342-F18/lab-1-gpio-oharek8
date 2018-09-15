c00dcc323cc81ac8b84d798131486a7f15b210a9
Kevin Ohare <oharek8@students.rowan.edu>
2018-09-15 13:17:59 -0400

    Multple Blink F5529 Commit

---
+++ Multiple Blink/MSP430F5529/main.c
@@ -0,0 +1,39 @@
+#include <msp430.h>
+/*
+ * main.c
+ */
+    int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P1DIR |= BIT0;
+	P4DIR |= BIT7;
+
+
+	int count1;
+	int count2;
+
+	while(1)
+	{
+	    if(count1 > 20000)
+	    {
+	        P1OUT ^= BIT0;
+
+	        count1 = 0;
+	    }
+	    if(count2 > 10000)
+	    {
+	        P4OUT ^= BIT7;
+
+	        count2 = 0;
+	    }
+
+
+	    count1++;
+	    count2++;
+	}
+
+
+
+	return 0;
+}
