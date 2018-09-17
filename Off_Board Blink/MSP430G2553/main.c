f04d538c1e32aa17c536ed2428ff88314d19c3fa
Kevin Ohare <oharek8@students.rowan.edu>
2018-09-17 15:35:21 -0400

    Off_Board Update

--- Off_Board Blink/MSP430G2553/main.c
+++ Off_Board Blink/MSP430G2553/main.c
@@ -9,15 +9,26 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	P1DIR |= BIT7;
+	P1DIR |= BIT6;
 
 	int i;
+	int j;
 
 	while(1)
 	{
-	    P1OUT ^= BIT7;
+	    if(i > 15000)
+	    {
+	        P1OUT ^= BIT7;
+	        i = 0;
+	    }
+	    if(j > 25000)
+	    {
+	        P1OUT ^= BIT6;
+	        j=0;
+	    }
 
-
-	    for(i = 0; i < 20000; i++);
+	    i++;
+	    j++;
 	}
 
 	return 0;
