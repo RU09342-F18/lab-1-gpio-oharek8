11724adf1155122d4754859aa5f27a6351c12702
oharek8 <35497829+oharek8@users.noreply.github.com>
2018-09-18 14:31:58 -0400

    Update main.c

--- Simple Blink/MSP430G2553/main.c
+++ Simple Blink/MSP430G2553/main.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	P1DIR |= BIT0;
-	P1SEL &= ~BIT0;
-	P1SEL2 &= ~BIT0;
+	P1DIR |= BIT0; //Set pin 1.0 to Output
+	//P1SEL &= ~BIT0;//
+	//P1SEL2 &= ~BIT0;
 
 	while(1)
 	{
-	    P1OUT ^= BIT0;
+	    P1OUT ^= BIT0;//Toggle LED on and off after each delay
 
-	    __delay_cycles(200000);
+	    __delay_cycles(200000);//Delays next toggle
 	}
 
 	return 0;
