d05f130579283a2001bbbf61e16bc5da1d99ece2
oharek8 <35497829+oharek8@users.noreply.github.com>
2018-09-19 10:21:33 -0400

    Update main.c

--- Off_Board Blink/MSP430G2553/main.c
+++ Off_Board Blink/MSP430G2553/main.c
@@ -8,27 +8,27 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	P1DIR |= BIT7;
-	P1DIR |= BIT6;
+	P1DIR |= BIT7; //Set pin 1.7 to Output
+	P1DIR |= BIT6; //Set pin 1.6 to Output
 
-	int i;
-	int j;
+	int i; //Counter Variable for pin 1.7 rate
+	int j; //Counter Variable for pin 1.6 rate
 
 	while(1)
 	{
 	    if(i > 15000)
 	    {
-	        P1OUT ^= BIT7;
-	        i = 0;
+	        P1OUT ^= BIT7; //Toggles LED after count exceeds 15000
+	        i = 0; //Resets counter to 0
 	    }
 	    if(j > 25000)
 	    {
-	        P1OUT ^= BIT6;
-	        j=0;
+	        P1OUT ^= BIT6; //Toggles LED after count exceeds 25000
+	        j=0; //Resets counter to 0
 	    }
 
-	    i++;
-	    j++;
+	    i++; //Increase count
+	    j++; //Increase count
 	}
 
 	return 0;
